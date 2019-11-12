de2ff4c046b529aea4b6175057b4fd68cdd4d5d9
sethferrell <sethferrell@gmail.com>
2019-11-11 21:37:35 -0800

    monday--first

---
+++ pythonsend.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import serial
+
+com = serial.Serial('/dev/tnt1')
+
+textfile = open('scipt.txt', 'r')
+data = textfile.read()
+
+
+no = com.write(data)
+
+textfile.close()
+
+print(com.name)
