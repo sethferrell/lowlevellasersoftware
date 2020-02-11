35b1a850c7ce3eea1a0fb2436148fb0597b08fda
sethferrell <45112803+sethferrell@users.noreply.github.com>
2020-02-10 17:22:00 -0800

    Update pythonsendDRAFT.py

--- draft/pythonsendDRAFT.py
+++ draft/pythonsendDRAFT.py
@@ -5,7 +5,7 @@ import os, sys
 
 class portSetup():
 	def __init__(self, port):
-		self.com = serial.Serial(port, 115200)
+		self.com = serial.Serial(port, 115200)	#Can we make the baudrate higher?
 		
 	def sendData(self, data):
 		data += "\r\n"
@@ -19,8 +19,8 @@ def main(ascii, portname):
 			print(data)
 
 if __name__ == '__main__':
-	ascii = sys.argv[1]
-	portname = sys.argv[2]
-	main(ascii, portname)
+	ascii = sys.argv[1] 		#Set to the file path of the attached "IRV" ascii document
+	portname = sys.argv[2]		#Set to the USB port connected to Linux ex: /dev/USB0
+	main(ascii, portname)		#Launch program as a command in Linux, call the file and give the two argument listed above
 
 
